4f5a44dd95c4bf5537c8e88fec955404dad3849b
Kshitij Nagdeote <89745710+k8ion@users.noreply.github.com>
2022-08-23 23:19:39 +0530

    Add files via upload

---
+++ LeetCode/234. Palindrome Linked List.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/palindrome-linked-list/
+
+class Solution {
+public:
+    ListNode* temp;
+    
+    bool check(ListNode* head) {
+        if (NULL == head) return true;
+        bool res = check(head->next) & (temp->val == head->val);
+        temp = temp->next;
+        return res;
+    }
+    
+    bool isPalindrome(ListNode* head) {
+        temp = head;
+        return check(head);
+    }
+};
