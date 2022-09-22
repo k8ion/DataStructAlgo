f0d03f88ce1fe0630c3e6572df8fe051ed34d57f
Kshitij Nagdeote <89745710+k8ion@users.noreply.github.com>
2022-09-22 12:31:31 +0530

    Add files via upload

---
+++ LeetCode/557. Reverse Words in a String III.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/reverse-words-in-a-string-iii/
+class Solution {
+public:
+    string reverseWords(string s) {
+        int l=0,r=0;
+        
+        while(l<s.length()){
+            while(r<s.size()&&s[r]!=' '){
+                r++;
+            }
+            
+            reverse(s.begin()+l,s.begin()+r);
+            l=r+1;
+            r=l;
+        }
+        return s;
+        
+    }
+};
